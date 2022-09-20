da7ea22bf4ce297650d9c2f8878d053f8585f89a
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-09-20 22:19:13 +0545

    sample check in Welcome method with parameters

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //Get:/HelloWorld/
+
+        
+        public string Index()
+        {
+            return "This is my default action....";
+        }
+
+        //
+        //GET: /Helloworld/Welcome/
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
